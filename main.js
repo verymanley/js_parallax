3ecde1d885937eca082f43f8af839d013195962e
Joshua Manley <verymanley@protonmail.com>
2020-04-07 18:05:58 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll", function ()
+{
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset
+  * 0.4 + "px";
+})
+
+
+
+const parallax = document.querySelectorAll(".parallax");
+
+window.addEventListener("scroll", function()
+{
+  let offset = window.pageYOffset;
+  parallax.forEach(function(prllx, i) {
+  prllx.style.backgroundPositionY = (offset - prllx.offsetTop) * 0.4 + "px";
+  })
+})
